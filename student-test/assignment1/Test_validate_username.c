07efcdff0bdd86a3839ba5699f3c897605da431f
Elfeshawy <kareemelfeshawy0@gmail.com>
2023-12-22 19:22:38 +0200

    Write SystemCalls.c

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -18,5 +18,13 @@ void test_validate_my_username()
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
      */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    //TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+        // Step 1: Call my_username() in autotest-validate.c
+    const char* hardcodedUsername = my_username();
+
+    // Step 2: Obtain the value from malloc_username_from_conf_file()
+    const char* confFileUsername = malloc_username_from_conf_file();
+
+    // Step 3: Use Unity assertion to verify equality
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(hardcodedUsername, confFileUsername, "AESD students, please fix me!");
 }
